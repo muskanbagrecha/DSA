8186904445bd1af1c2c4d159e2b70a4d77f29c5d
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-01-07 23:28:44 +0530

    Bitwise operations

--- src/main/java/org/example/BitwiseOperations.java
+++ src/main/java/org/example/BitwiseOperations.java
@@ -1,2 +1,139 @@
-package org.example;public class BitwiseOperations {
+package org.example;
+
+public class BitwiseOperations {
+
+    /*
+    1. If last bit is odd => num is odd and vice versa
+    2. & with 1 will return the last bit as it is
+     example: 101001 & 000001 = 1, 100 & 001 = 0
+     */
+    public static String findOddOrEven(int num) {
+        return (num & 1) == 1 ? "Odd" : "Even";
+    }
+
+    /*
+    Input: [1,1,5,7,5,10,9,9,10]
+    Output: 7
+    Note: here all elements but one are occurring twice. Solution is same
+    for any even number of occurrence.
+    XOR any number by itself we will get 0.
+    So we are going through all elements in the array and ultimately they get
+    cancelled out except the one unique element.
+    Note order does not matter due to associative property
+    (a^b)^c=a^(b^c)
+    */
+    public static int findUniqueElement(int[] arr) {
+        int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            num ^= arr[i];
+        }
+        return num;
+    }
+
+    /*
+    100 & 010 = 0 (to find 1st bit)
+    100 & 100 = 1 (to find 2nd bit)
+    to get the right hand operand => 1 << i => 1<<2 = 100
+    to get the bit finally => 100 >> i = 1
+    */
+    public static int findIthBitOfANumber(int num, int i) {
+        return (num & (1 << i)) >> i;
+    }
+
+    /*
+    Given: 100, set 2nd bit
+    Output: 100
+
+    Given: 100, set 1st bit
+    Output: 110
+    */
+    public static int setTheIthBit(int num, int i) {
+        return (num | (1 << i));
+    }
+
+    /*
+    Given: 100, reset 2nd bit
+    Mask: 011 (complement of 100)
+    Output: 000
+
+    Given: 100, reset 1st bit
+    Mask: 101 (complement of 010)
+    Output: 100
+    */
+    public static int resetIthBit(int num, int i) {
+        int mask = ~(1 << i);
+        return (num & mask);
+    }
+
+    //TODO
+    /*Find the position of the right most set bit*/
+    /*
+    Input: 10110110
+    Output: 1
+    */
+
+    //TODO
+    /*
+    Find the unique element when all other elements occur 3 (or any odd
+    number of times)
+    Input: [1,4,1,2,1,2,2]
+    Output: 4
+    */
+    public static int findUnique(int[] arr){
+        int sum = 0;
+        for(int i = 0; i<arr.length;i++){
+            sum += arr[i];
+        }
+        int n = (int)(Math.log(sum)/Math.log(2)) + 1;
+        int mask = ~(1>>n+1);
+        System.out.println(sum%mask);
+        return sum%mask;
+    }
+
+    /*
+    Find nth magic number
+    1: 0 0 1 => 1 * 5**1
+    2: 0 1 0 => 1 * 5**2
+    3: 0 1 1 => 1 * 5**2 + 1 * 5**1
+    4: 1 0 0 => 1 * 5**3
+    5: 1 0 1 => 1 * 5**3 + 1 * 5**1
+    */
+    public static double magicNumber(int n){
+        double sum = 0;
+        int i = 1;
+        while(n>0){
+            int lastBit = n&1;
+            sum+=(lastBit * Math.pow(5, i));
+            i++;
+            n=n>>1;
+         }
+        return sum;
+    }
+
+    public static int numberOfDigits(int num, int base){
+        return (int)Math.floor(Math.log(num)/Math.log(base)) + 1; //no need to add math.floor()
+    }
+
+    /*
+    Number is a power of two if in binary rep it is 1 followed by all 0s
+    Ex: 100, 10, 10000
+    */
+//    public static boolean powerOfTwo(int num){
+//        int mask = ~(1 << (int)Math.floor(Math.log(num)/Math.log(2)));
+//        return (num & mask) == 0 ? true: false;
+//    }
+        public static boolean powerOfTwo(int num){
+            if(num==0){
+                return false;
+            }
+            return (num & num-1) == 0 ? true : false;
+            /*
+            100 = 11 + 1
+            n = (n-1) + 1
+              1 0 0 --> n
+            & 0 1 1 --> n-1
+            = 0 0 0
+             */
+    }
+
 }
