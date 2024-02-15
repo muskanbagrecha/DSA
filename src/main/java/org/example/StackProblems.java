95d503969a4eb9ed439608184fe15d887e896e70
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-02-15 14:07:00 +0530

    Stacks: reverse a stack

--- src/main/java/org/example/StackProblems.java
+++ src/main/java/org/example/StackProblems.java
@@ -24,4 +24,27 @@ public class StackProblems {
 
     //Time Complexity: O(n)
     //Space Complexity: O(n) - recursion stack
+
+    //Reverse a stack
+    public static void reverse(Stack<Integer> stack, int n){
+        if(n==1){
+            return; //already sorted.
+        }
+        Integer element = stack.pop();
+        insert(stack, element, n-1);
+        reverse(stack, n-1);
+    }
+
+    public static void insert(Stack<Integer> stack, Integer data, int index){
+        if(index==0){
+            stack.push(data);
+            return;
+        }
+        int top = stack.pop();
+        insert(stack, data, index-1);
+        stack.push(top);
+    }
+
+    //Time complexity: O(n**2)
+    //Space: O(n) - recursion stack
 }
