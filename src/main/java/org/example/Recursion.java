a0206ebac106936f6162d64c8500a5741058e13c
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-02-15 14:07:41 +0530

    Recursion: reverse a stack using recursion

--- src/main/java/org/example/Recursion.java
+++ src/main/java/org/example/Recursion.java
@@ -111,4 +111,28 @@ public class Recursion {
 
     //Time: O(n**2)
     //Space: O(n)
+
+    //Reverse a stack
+    public static void reverse(Stack<Integer> stack, int n){
+        if(n==1){
+            return; //already sorted.
+        }
+        Integer element = stack.pop();
+        insert(stack, element, n-1);
+        reverse(stack, n-1);
+    }
+
+    public static void insert(Stack<Integer> stack, Integer data, int index){
+        if(index==0){
+            stack.push(data);
+            return;
+        }
+        int top = stack.pop();
+        insert(stack, data, index-1);
+        stack.push(top);
+    }
+
+    //Time complexity: O(n**2)
+    //Space: O(n) - recursion stack
+
 }
