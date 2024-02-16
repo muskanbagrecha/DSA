97360ee5c290835759929d5253ffc607cc1e2482
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-02-16 09:33:58 +0530

    Binary tree: pre-order traversal

--- src/main/java/org/example/BinaryTreeProblems.java
+++ src/main/java/org/example/BinaryTreeProblems.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.List;
+
 public class BinaryTreeProblems {
 
     //    https://www.geeksforgeeks.org/problems/height-of-binary-tree/1
@@ -15,4 +17,20 @@ public class BinaryTreeProblems {
     // Space: O(n) - recursion stack
     // In the worst case (e.g., a skewed tree), the recursion stack could have n function calls, leading to a space complexity of O(n).
     // For a balanced tree, the space complexity would be O(log n) due to the height of the tree, but the worst-case is considered O(n).
+
+    //Traversals
+
+    //Pre-Order Traversal
+    //root-left-right
+    public void preOrderTraversal(TreeNode root, List<Integer> list){
+        if(root==null){
+            System.out.println(" END ");
+            return;
+        }
+        System.out.println(root.data + " -> ");
+        preOrderTraversal(root.left, list);
+        preOrderTraversal(root.right, list);
+    }
+
+    //O(n) time and O(n) due to stack - in iterative we can do in O(1) time.
 }
