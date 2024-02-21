79d4f95af184d5fecba2b2fb4195f5ba45fd202b
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-02-22 00:50:48 +0530

    next larger element

--- src/main/java/org/example/StackProblems.java
+++ src/main/java/org/example/StackProblems.java
@@ -5,21 +5,24 @@ import java.util.Stack;
 
 public class StackProblems {
 
+    //it's worth noting that for the stack, you could also use `Deque<Integer>` as a stack, with `LinkedList` implementation,
+    //because the `Stack` class is considered somewhat outdated. `Deque` is a more complete and consistent set of LIFO stack operations
+
 //    https://www.geeksforgeeks.org/problems/delete-middle-element-of-a-stack/
 
     public static void deleteMiddle(Stack<Integer> inputStack, int N) {
         // WRITE YOUR CODE HERE
-        int k = N%2==0 ? (N/2)-1: N/2;
+        int k = N % 2 == 0 ? (N / 2) - 1 : N / 2;
         delete(inputStack, k);
     }
 
-    public static void delete(Stack<Integer> stack, int k){
-        if(k==0){
+    public static void delete(Stack<Integer> stack, int k) {
+        if (k == 0) {
             stack.pop();
             return;
         }
         Integer temp = stack.pop();
-        delete(stack, k-1);
+        delete(stack, k - 1);
         stack.push(temp);
     }
 
@@ -27,22 +30,22 @@ public class StackProblems {
     //Space Complexity: O(n) - recursion stack
 
     //Reverse a stack
-    public static void reverse(Stack<Integer> stack, int n){
-        if(n==1){
+    public static void reverse(Stack<Integer> stack, int n) {
+        if (n == 1) {
             return; //already sorted.
         }
         Integer element = stack.pop();
-        insert(stack, element, n-1);
-        reverse(stack, n-1);
+        insert(stack, element, n - 1);
+        reverse(stack, n - 1);
     }
 
-    public static void insert(Stack<Integer> stack, Integer data, int index){
-        if(index==0){
+    public static void insert(Stack<Integer> stack, Integer data, int index) {
+        if (index == 0) {
             stack.push(data);
             return;
         }
         int top = stack.pop();
-        insert(stack, data, index-1);
+        insert(stack, data, index - 1);
         stack.push(top);
     }
 
@@ -53,7 +56,7 @@ public class StackProblems {
     public static boolean isValid(String s) {
         char[] charArray = s.toCharArray();
         int n = charArray.length;
-        if(n%2!=0){
+        if (n % 2 != 0) {
             return false;
         }
         Stack<Character> stack = new Stack<>();
@@ -61,14 +64,13 @@ public class StackProblems {
         map.put('(', ')');
         map.put('{', '}');
         map.put('[', ']');
-        for(int i = 0; i<n; i++){
-            if(map.get(charArray[i])==null){
-                if(!stack.isEmpty() && map.get(stack.peek())-charArray[i]==0){
+        for (int i = 0; i < n; i++) {
+            if (map.get(charArray[i]) == null) {
+                if (!stack.isEmpty() && map.get(stack.peek()) - charArray[i] == 0) {
                     stack.pop();
                 }
                 return false;
-            }
-            else{
+            } else {
                 stack.push(charArray[i]);
             }
         }
@@ -82,20 +84,19 @@ public class StackProblems {
     public static boolean isValid2(String s) {
         char[] charArray = s.toCharArray();
         int n = charArray.length;
-        if(n%2!=0){ // just for early return
+        if (n % 2 != 0) { // just for early return
             return false;
         }
         Stack<Character> stack = new Stack<>();
-        for(int i = 0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             char c = charArray[i];
-            if(c==')' || c=='}' || c==']'){
-                if(!stack.isEmpty() && (c-stack.peek()==1 || c-stack.peek()==2)){
+            if (c == ')' || c == '}' || c == ']') {
+                if (!stack.isEmpty() && (c - stack.peek() == 1 || c - stack.peek() == 2)) {
                     stack.pop();
                     continue;
                 }
                 return false;
-            }
-            else{
+            } else {
                 stack.push(charArray[i]);
             }
         }
@@ -120,4 +121,24 @@ public class StackProblems {
         // so the string is valid, otherwise, there are unmatched opening brackets, so return false
         return stack.isEmpty();
     }
+
+    //https://www.geeksforgeeks.org/problems/next-larger-element-1587115620
+    public static long[] nextLargerElement(long[] arr, int n) {
+        // Your code here
+        Stack<Long> s = new Stack<>();
+        long[] ans = new long[n];
+        for (int i = n - 1; i >= 0; i--) {
+            while (!s.empty() && s.peek() <= arr[i]) {
+                s.pop();
+            }
+            ans[i] = s.empty()? -1 : s.peek();
+            s.push(arr[i]);
+        }
+        return ans;
+    }
+    //Time & space: O(n)
+    //Note: pushing of any element happens only once and popping will also happen at most once.
+    // So, over the course of the entire function, there will be exactly n pushes and at most n pops in total.
+    // This means that the stack operations (pushing and popping) are amortized over the n iterations, resulting in an average cost of O(1) per operation.
+    //So total time complexity would be only O(n) for iterative over the original array.
 }
