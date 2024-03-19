e96e20d5388d225bec501e8c8a2c5cd7b7f73a34
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-03-19 23:16:50 +0530

    Binary tree implementation

--- src/main/java/org/example/trees/BinaryTree.java
+++ src/main/java/org/example/trees/BinaryTree.java
@@ -1,6 +1,8 @@
 package org.example.trees;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class BinaryTree {
@@ -17,32 +19,58 @@ public class BinaryTree {
     }
 
     //TODO: improve insertion method to be more streamlined and creating a version of programmatic insertion
-    public void insert() {
-        System.out.println("Enter root node");
-        int data = scanner.nextInt();
-        this.root = new TreeNode(data);
-        insertChildren(root);
-    }
+//    public void insert() {
+//        System.out.println("Enter root node");
+//        int data = scanner.nextInt();
+//        this.root = new TreeNode(data);
+//        insertChildren(root);
+//    }
 
-    private void insertChildren(TreeNode node){
-        System.out.println("Do you want to insert to the left? Enter true or false.");
-        boolean left = scanner.nextBoolean();
-        if(left){
-            System.out.println("Enter the data value of left");
-            int leftData = scanner.nextInt();
-            TreeNode leftNode = new TreeNode(leftData);
-            node.left = leftNode;
-            insertChildren(leftNode);
+//    private void insertChildren(TreeNode node){
+//        System.out.println("Do you want to insert to the left? Enter true or false.");
+//        boolean left = scanner.nextBoolean();
+//        if(left){
+//            System.out.println("Enter the data value of left");
+//            int leftData = scanner.nextInt();
+//            TreeNode leftNode = new TreeNode(leftData);
+//            node.left = leftNode;
+//            insertChildren(leftNode);
+//        }
+//        System.out.println("Do you want to insert to the right? Enter true or false.");
+//        boolean right = scanner.nextBoolean();
+//        if(right){
+//            System.out.println("Enter the data value of right");
+//            int rightData = scanner.nextInt();
+//            TreeNode rightNode = new TreeNode(rightData);
+//            node.right = rightNode;
+//            insertChildren(rightNode);
+//        }
+//    }
+
+    public static TreeNode insertLevelOrder(Integer[] array){
+        if (array == null || array.length == 0) return null;
+        TreeNode root = new TreeNode(array[0]);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int i = 1;
+        while(!q.isEmpty() && i<array.length){
+            TreeNode curr = q.poll();
+            if(curr!=null && curr.left==null){
+                if(array[i]!=null){
+                    curr.left = new TreeNode(array[i]);
+                    q.add(curr.left);
+                }
+                i++;
+            }
+            if(curr!=null && curr.right==null){
+                if(array[i]!=null){
+                    curr.right = new TreeNode(array[i]);
+                    q.add(curr.right);
+                }
+                i++;
+            }
         }
-        System.out.println("Do you want to insert to the right? Enter true or false.");
-        boolean right = scanner.nextBoolean();
-        if(right){
-            System.out.println("Enter the data value of right");
-            int rightData = scanner.nextInt();
-            TreeNode rightNode = new TreeNode(rightData);
-            node.right = rightNode;
-            insertChildren(rightNode);
-        }
+        return root;
     }
 
     //Pre-Order Traversal
@@ -55,6 +83,10 @@ public class BinaryTree {
         System.out.println(root.data + " -> ");
         preOrderTraversal(root.left, list);
         preOrderTraversal(root.right, list);
+    }
+
+    public void levelOrderTraversal(TreeNode root, List<Integer> list){
+
     }
 }
 
