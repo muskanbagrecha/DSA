06a69ee3e0a9adc816fbbeb785febc4fcce3ff84
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-03-23 16:14:10 +0530

    Graph Traversal: BFS time & space analysis

--- src/main/java/org/example/graphs/GraphProblems.java
+++ src/main/java/org/example/graphs/GraphProblems.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class GraphProblems {
-    public static ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+    public static ArrayList<Integer> bfsOfDirectedGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
         boolean[] vis = new boolean[V];
         ArrayList<Integer> ans = new ArrayList<>();
@@ -24,4 +24,10 @@ public class GraphProblems {
         }
         return ans;
     }
+    //Space: O(V) for queue + O(V) for visited array + O(V) storage of output
+    //Time: While will run for V times at max (each node is pushed into queue only once) + total degrees (for the inner for loop)
+    //Total degrees = no of edges as it is directed graph
+    //Total time: O(V) + O(E) = O(V+E)
+
+    //So, for a directed graph, the overall time complexity of the BFS traversal remains O(V + E), acknowledging that each edge is considered once in its directed context, as opposed to the undirected scenario where each edge was considered twice (once from each of its two vertices).
 }
