1631717f48b4503a93ad274b6310294bb0664886
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-03-23 16:37:16 +0530

    Graph Traversal: DFS with time & space analysis

--- src/main/java/org/example/graphs/GraphProblems.java
+++ src/main/java/org/example/graphs/GraphProblems.java
@@ -30,4 +30,25 @@ public class GraphProblems {
     //Total time: O(V) + O(E) = O(V+E)
 
     //So, for a directed graph, the overall time complexity of the BFS traversal remains O(V + E), acknowledging that each edge is considered once in its directed context, as opposed to the undirected scenario where each edge was considered twice (once from each of its two vertices).
+
+    //https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
+    public ArrayList<Integer> dfsOfUnconnectedGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean[] vis = new boolean[V];
+        dfsRecursionHelper(0, adj, vis, ans);
+        return ans;
+    }
+
+    public void dfsRecursionHelper(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis, ArrayList<Integer> ans){
+        vis[node] = true;
+        ans.add(node);
+        for(int neighbour : adj.get(node)){
+            if(!vis[neighbour]){
+                dfsRecursionHelper(neighbour, adj, vis, ans);
+            }
+        }
+    }
+    //Space: O(V) worst case when we have a skewed tree like structure (recursion stack) + O(V) for visited graph + O(V) for output array => O(V)
+    //Time: O(V) + O(2*E) => O(V+E)
 }
