547bcd91b653f42cb149f1f17d849d8142eb4936
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-03-23 16:01:11 +0530

    Graph Traversal: BFS

--- src/main/java/org/example/graphs/GraphProblems.java
+++ src/main/java/org/example/graphs/GraphProblems.java
@@ -1,2 +1,27 @@
-package org.example.graphs;public class GraphProblems {
+package org.example.graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class GraphProblems {
+    public static ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean[] vis = new boolean[V];
+        ArrayList<Integer> ans = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        vis[0] = true;
+        while(!q.isEmpty()){
+            int element = q.remove();
+            ans.add(element);
+            for(int neighbour : adj.get(element)){
+                if(vis[neighbour]!=true){
+                    vis[neighbour] = true;
+                    q.add(neighbour);
+                }
+            }
+        }
+        return ans;
+    }
 }
