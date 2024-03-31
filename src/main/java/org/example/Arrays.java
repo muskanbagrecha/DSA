ea69509f14ea92ca9cd80569e440279ccad10204
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-03-31 22:48:53 +0530

    arrays: island perimeter

--- src/main/java/org/example/Arrays.java
+++ src/main/java/org/example/Arrays.java
@@ -798,4 +798,23 @@ public class Arrays {
         }
         return 0;
     }
+
+    //https://leetcode.com/problems/island-perimeter
+    public int islandPerimeter(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int num = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    if (i == 0 || grid[i - 1][j] == 0) num++; // UP
+                    if (j == 0 || grid[i][j - 1] == 0) num++; // LEFT
+                    if (i == rows -1 || grid[i + 1][j] == 0) num++; // DOWN
+                    if (j == cols -1 || grid[i][j + 1] == 0) num++; // RIGHT
+                }
+            }
+        }
+        return num;
+    }
 }
