ba87d2b09b1a160dfb6aa2115c2c7d3ce2286b9d
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-04-02 00:30:56 +0530

    DP: Knapsack memoized

--- src/main/java/org/example/DynamicProgramming.java
+++ src/main/java/org/example/DynamicProgramming.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class DynamicProgramming {
     //https://www.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1
 
@@ -17,5 +19,31 @@ public class DynamicProgramming {
         else{
             return knapSack(W, wt, val, n-1);
         }
+    }
+
+    //Memoized
+    static int knapSackMemo(int W, int wt[], int val[], int n)
+    {
+        HashMap<String, Integer> map = new HashMap<>();
+        return knapSackMemoHelper(W, wt, val, n, map);
+    }
+
+    static int knapSackMemoHelper(int W, int wt[], int val[], int n, HashMap<String, Integer> map){
+        if(n==0 || W==0){
+            return 0;
+        }
+        String key = n + "_" + W;
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
+        int profit = 0;
+        if(wt[n-1]<=W){
+            profit =  Math.max(val[n-1] + knapSackMemoHelper(W-wt[n-1], wt, val, n-1, map), knapSackMemoHelper(W, wt, val, n-1, map));
+        }
+        else{
+            profit = knapSackMemoHelper(W, wt, val, n-1, map);
+        }
+        map.put(key, profit);
+        return profit;
     }
 }
