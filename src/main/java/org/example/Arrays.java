88b5b0f4f65f3c25457236e63ff327ae409b3840
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-04-30 09:10:35 +0530

    two sum

--- src/main/java/org/example/Arrays.java
+++ src/main/java/org/example/Arrays.java
@@ -630,6 +630,16 @@ public class Arrays {
     }
     //Time: O(n)
     //Space: O(n)
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target-nums[i], i);
+        }
+        return new int[]{0, 1};
+    }
 
 //    https://leetcode.com/problems/majority-element/description/
 //    Given an array nums of size n, return the majority element.
