70c4abb122f2a54531dab3335a4cfe079742fe67
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-05-03 09:22:27 +0530

    Binary tree: bottom view of a tree

--- src/main/java/org/example/trees/BinaryTreeProblems.java
+++ src/main/java/org/example/trees/BinaryTreeProblems.java
@@ -870,6 +870,40 @@ public class BinaryTreeProblems {
 
     //After the BFS completes, you iterate over the range of columns from minColumn to maxColumn to construct the final result list. Let K be the number of distinct columns that have at least one node visible from the top view (K could theoretically be as small as 1 or as large as N, but typically K will be much less than N for most trees except in cases like a perfectly straight line).
     //Time: O(n) + O(k) => O(n)
+
+    public ArrayList<Integer> bottomView(Node root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Map<Integer, Integer> columnTable = new HashMap<>();
+        Queue<NodeColumnPair> queue = new LinkedList<>();
+        queue.offer(new NodeColumnPair(root, 0));
+        int minColumn = 0, maxColumn = 0;
+
+        while (!queue.isEmpty()) {
+            NodeColumnPair curr = queue.poll();
+            int column = curr.column;
+            // Overwrite the map entry at every column with the current node's data
+            columnTable.put(column, curr.node.val);
+
+            minColumn = Math.min(minColumn, column);
+            maxColumn = Math.max(maxColumn, column);
+
+            if (curr.node.left != null) {
+                queue.offer(new NodeColumnPair(curr.node.left, column - 1));
+            }
+            if (curr.node.right != null) {
+                queue.offer(new NodeColumnPair(curr.node.right, column + 1));
+            }
+        }
+
+        // Collect results from the hash map in order of their columns
+        for (int i = minColumn; i <= maxColumn; i++) {
+            res.add(columnTable.get(i));
+        }
+        return res;
+    }
+    //Time: O(n) + O(k) where k is distinct columns => simplifies to O(n)
 }
 
 class NodeColumnPair{
