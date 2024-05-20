c4f1d622e05160d3dd06a5b445e11c586f45f7d5
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-05-20 09:08:10 +0530

    Heaps: Find k closest numbers

--- src/main/java/org/example/HeapProblems.java
+++ src/main/java/org/example/HeapProblems.java
@@ -1,7 +1,8 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.PriorityQueue;
+import org.example.pair.Pair;
+
+import java.util.*;
 
 public class HeapProblems {
     //Sort K nearly sorted array
@@ -24,4 +25,30 @@ public class HeapProblems {
     }
     //Time: O(klogk)
     //Space: O(K)
+
+    //Find K closest elements
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> res = new ArrayList<>();
+        PriorityQueue<Pair> pq = new PriorityQueue<>(new ClosestElementsComparator());
+        for (int i = 0; i < arr.length; i++) {
+            pq.add(new Pair(Math.abs(arr[i] - x), arr[i]));
+            if (pq.size() > k) {
+                pq.remove();
+            }
+        }
+        pq.forEach(pair -> res.add(pair.second));
+        Collections.sort(res);
+        return res;
+    }
+    //TC: O(n log k) + O(k) + O(klogk) => O(nlogk) + O(klogk)
+}
+
+class ClosestElementsComparator implements Comparator<Pair> {
+    @Override
+    public int compare(Pair a, Pair b) {
+        if (a.first != b.first) {
+            return b.first - a.first; // Max-Heap: Larger differences come first
+        }
+        return b.second - a.second; // For same differences, larger numbers come first
+    }
 }
