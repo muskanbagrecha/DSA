9be36df48ff634138b959f04939db00cbc86140a
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-05-28 19:14:41 +0530

    BS on answer: allocate min no of pages

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -5,7 +5,59 @@ public class BinarySearch {
     //input: 2, 4, 10, 10, 10, 18, 20
     //element: 10
     //output: 2
-    //https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+    //https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+    //Method 1:
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        if(first==-1){
+            return new int[]{-1, -1};
+        }
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+
+    public int findFirst(int[] nums, int target){
+        int low = 0;
+        int high = nums.length-1;
+        int first = -1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(target==nums[mid]){
+                first = mid;
+                high = mid-1;
+            }
+            else if(target<nums[mid]){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return first;
+    }
+
+    public int findLast(int[] nums, int target){
+        int low = 0;
+        int high = nums.length-1;
+        int last = -1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(target==nums[mid]){
+                last = mid;
+                low = mid+1;
+            }
+            else if(target<nums[mid]){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return last;
+    }
+
+    //Method2
     public static int firstOccurenceOfElement(int[] arr, int element) {
         int n = arr.length;
         int start = 0;
@@ -800,4 +852,7 @@ public class BinarySearch {
         }
         return true;
     }
+
+    //https://www.geeksforgeeks.org/allocate-minimum-number-pages/
+
 }
