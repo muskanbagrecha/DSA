8a528fe242b7edbdc6df695ff209d9045a660885
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-06-17 00:01:03 +0530

    Arrays: Rotate array by 90 degrees - clockwise

--- src/main/java/org/example/Arrays.java
+++ src/main/java/org/example/Arrays.java
@@ -1029,4 +1029,25 @@ public class Arrays {
         }
         return slow;
     }
+    //https://leetcode.com/problems/rotate-image/
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int top = 0;
+        int bottom = n - 1;
+        while (top < bottom) {
+            int temp[] = matrix[top];
+            matrix[top] = matrix[bottom];
+            matrix[bottom] = temp;
+            top++;
+            bottom--;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
 }
