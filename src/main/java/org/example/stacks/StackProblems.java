dfd41c32a1922181e9ec7d7aceccb930f8887c34
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2024-06-17 00:21:36 +0530

    Stacks: Daily temperatures

--- src/main/java/org/example/stacks/StackProblems.java
+++ src/main/java/org/example/stacks/StackProblems.java
@@ -415,4 +415,23 @@ public class StackProblems {
         }
         return ans;
     }
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        Stack<Integer> s = new Stack<>();
+        for(int i = n-1; i>=0; i--){
+            while(!s.isEmpty() && temperatures[s.peek()]<=temperatures[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                res[i] = 0;
+            }
+            else{
+                res[i] = s.peek()-i;
+            }
+            s.add(i);
+        }
+        return res;
+    }
 }
