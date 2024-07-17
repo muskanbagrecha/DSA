d2ad24f1d88c04d7ae87cc40ec11b6634b0c9d67
muskanbagrecha <muskanbagrecha04@gmail.com>
2024-07-17 09:56:54 +0530

    Binary tree: LCA

--- src/main/java/org/example/trees/BinaryTreeProblems.java
+++ src/main/java/org/example/trees/BinaryTreeProblems.java
@@ -1012,6 +1012,21 @@ public class BinaryTreeProblems {
         }
         return null;
     }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null || root==p || root==q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left!=null && right!=null){
+            return root;
+        }
+        if(right==null){
+            return left;
+        }
+        return right;
+    }
 }
 
 class NodeColumnPair{
