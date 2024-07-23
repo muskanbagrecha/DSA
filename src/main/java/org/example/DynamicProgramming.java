3fbea3e060ed37a1f5507c5b37af5e8189070ab2
muskanbagrecha <muskanbagrecha04@gmail.com>
2024-07-23 21:54:46 +0530

    DP: geek's training

--- src/main/java/org/example/DynamicProgramming.java
+++ src/main/java/org/example/DynamicProgramming.java
@@ -562,4 +562,51 @@ public class DynamicProgramming {
         return curr;
     }
     //SC: O(1)
+
+    public int maximumPoints(int points[][], int N) {
+        // code here
+        int[][] dp = new int[N+1][3];
+        for(int i = 0; i<=N; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return findMaxPoints(points, N, -1, dp);
+    }
+
+    public int findMaxPoints(int points[][], int N, int prev, int[][] dp){
+        if(prev!=-1 && dp[N][prev]!=-1){
+            return dp[N][prev];
+        }
+        if(N==0){
+            return 0;
+        }
+        int temp = 0;
+        for(int i = 0; i<3; i++){
+            if(i!=prev){
+                temp = Math.max(temp, points[N-1][i] + findMaxPoints(points, N-1, i, dp));
+            }
+        }
+        if(prev!=-1){
+            dp[N][prev] = temp;
+        }
+        return temp;
+    }
+
+    //Tabulation
+    public int maximumPointsTabulation(int points[][], int N) {
+        // code here
+        int[][] dp = new int[N][3];
+        dp[0][0] = points[0][0];
+        dp[0][1] = points[0][1];
+        dp[0][2] = points[0][2];
+        for(int i = 1; i<N; i++){
+            dp[i][0] = points[i][0] + Math.max(dp[i-1][1], dp[i-1][2]);
+            dp[i][1] = points[i][1] + Math.max(dp[i-1][0], dp[i-1][2]);
+            dp[i][2] = points[i][2] + Math.max(dp[i-1][0], dp[i-1][1]);
+        }
+        int temp = 0;
+        for(int i = 0; i<3; i++){
+            temp = Math.max(temp, dp[N-1][i]);
+        }
+        return temp;
+    }
 }
