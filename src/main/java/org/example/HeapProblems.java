94922fb59cb449a1610372d8ef9a4dd7a693a28c
muskanbagrecha <muskanbagrecha04@gmail.com>
2024-08-12 10:18:52 +0530

    heaps: top k freq element

--- src/main/java/org/example/HeapProblems.java
+++ src/main/java/org/example/HeapProblems.java
@@ -66,6 +66,29 @@ public class HeapProblems {
         }
         return res;
     }
+
+    //https://leetcode.com/problems/top-k-frequent-elements/
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        int[] res = new int[k];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1]-b[1]);
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            pq.add(new int[]{entry.getKey(), entry.getValue()});
+            if(pq.size()>k){
+                pq.remove();
+            }
+        }
+        while(!pq.isEmpty()){
+            res[res.length-k] = pq.remove()[0];
+            k--;
+        }
+        return res;
+    }
+
+    //TC: O(nlogk)
 }
 
 class ClosestElementsComparator implements Comparator<Pair> {
