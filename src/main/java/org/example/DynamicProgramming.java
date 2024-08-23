3cb994f04483e44e420eb2a2cd3d9589a69da73e
muskanbagrecha <muskanbagrecha04@gmail.com>
2024-08-23 22:16:25 +0530

    LCS: tabulation

--- src/main/java/org/example/DynamicProgramming.java
+++ src/main/java/org/example/DynamicProgramming.java
@@ -447,6 +447,32 @@ public class DynamicProgramming {
     //TC: O(M*N)
     //SC: O(M*N)
 
+    public int longestCommonSubsequenceTabulation(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 0; i<=m; i++){
+            dp[i][0] = 0;
+        }
+        for(int j = 0; j<=n; j++){
+            dp[0][j] = 0;
+        }
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=n; j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else if(dp[i-1][j]>dp[i][j-1]){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
     public int longestCommonSubsequence1D(String text1, String text2) {
         if (text2.length() > text1.length()) {
             String temp = text1;
