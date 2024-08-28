7ca0b8df442565db13c5866aa5f9c46940da8f48
muskanbagrecha <muskanbagrecha04@gmail.com>
2024-08-28 23:13:06 +0530

    Two sum sorted array

--- src/main/java/org/example/top150/Problems.java
+++ src/main/java/org/example/top150/Problems.java
@@ -277,4 +277,23 @@ public class Problems {
 
         return findSum(root.left, sum) + findSum(root.right, sum);
     }
+
+    //Two sum 2 - sorted array.
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.length-1;
+        while(i<j){
+            int sum = numbers[i] + numbers[j];
+            if(sum==target){
+                return new int[]{i+1, j+1};
+            }
+            if(sum>=target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return null;
+    }
 }
