89ed20e092d5192b7d574c57d8c82c6d321b6ee1
muskanbagrecha <muskanbagrecha04@gmail.com>
2024-08-29 16:32:52 +0530

    Jump 2

--- src/main/java/org/example/top150/Problems.java
+++ src/main/java/org/example/top150/Problems.java
@@ -296,4 +296,22 @@ public class Problems {
         }
         return null;
     }
+
+    //Jump 2 using Greedy
+    public int jump(int[] nums) {
+        int l = 0;
+        int r = 0;
+        int jumps = 0;
+        while(r<nums.length-1){
+            int farthest = 0;
+            for(int i = l; i<=r; i++){
+                farthest = Math.max(farthest, i + nums[i]);
+            }
+            jumps++;
+            l = r+1;
+            r = farthest;
+        }
+        return jumps;
+    }
+    //TC:: O(n) -- dp soln for this is O(n^2)
 }
