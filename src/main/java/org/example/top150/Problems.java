7e68b9edfd7852f2aaaba06a1cc67d24853f9937
muskanbagrecha <muskanbagrecha04@gmail.com>
2024-09-17 18:37:36 +0530

    LRS

--- src/main/java/org/example/top150/Problems.java
+++ src/main/java/org/example/top150/Problems.java
@@ -687,4 +687,27 @@ public class Problems {
         if(!rightValid) return false;
         return true;
     }
+
+    public int LongestRepeatingSubsequence(String str)
+    {
+        // code here
+        return lcs(str, str);
+    }
+
+    private int lcs(String s1, String s2){
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=n; j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1) && i!=j){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
 }
